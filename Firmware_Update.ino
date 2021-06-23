02708be04b400e3fde8d22daf041c047ae58b3c6
Darshnik253 <85985007+Darshnik253@users.noreply.github.com>
2021-06-23 16:09:16 +0530

    Add files via upload

--- Firmware_Update.ino
+++ Firmware_Update.ino
@@ -3,12 +3,47 @@
 #include <HTTPUpdate.h>
 #include <WiFiClientSecure.h>
 #include <FirebaseESP32.h>
+#include "FS.h"
+#include "SPIFFS.h"
 
 #define FIREBASE_HOST "automation-aa27f-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "vJ7ggGytnsU9JUHK5IdJH9G3SQlc0DNFczSn5HbQ"
 #define URL_fw_Version "https://raw.githubusercontent.com/Darshnik253/Firmware_Update/master/version.txt"
 #define URL_fw_Bin "https://raw.githubusercontent.com/Darshnik253/Firmware_Update/master/Firmware_Update.bin"
 #define ledPin 3
+#define FORMAT_SPIFFS_IF_FAILED true
+volatile byte touch = 0;
+static const int snormal = 6;
+static const int sdim = 0;
+volatile byte fq = 0, l = 0, lf = 0, le = 0, fa = 0, por = 0, c = 0, lef = 0, pf = 0, sigd1 = 0, sigd2 = 0, sig1 = 0, sig2 = 0,fail = 0;
+volatile int rep = 0, wup = 0, makeb = 0; // mqttconnect,74disable
+int fi = 0, ff = 0;
+void IRAM_ATTR handleInterrupt1();
+void IRAM_ATTR handleInterrupt2();
+void IRAM_ATTR handleInterrupt3();
+void IRAM_ATTR handleInterrupt4();
+void IRAM_ATTR handleInterrupt5();
+void IRAM_ATTR handleInterrupt6();
+void printResult(FirebaseData &data);
+void printResult(StreamData &data);
+void connect_wifi();
+void firmwareUpdate();
+int FirmwareVersionCheck();
+
+static const int lightin = 26;//D2
+static const int fanin = 25;//D1
+static const int ledin = 15;//D6
+static const int portin = 14;//d5
+static const int signalin1 = 13;//rx
+static const int signalin2 = 4;//tx
+
+static const int lightout = 23;//D2
+static const int fanout = 22;//D1
+static const int ledout = 21;//D6
+static const int portout = 19;//d5
+static const int signalout1 = 18;//rx
+static const int signalout2 = 5;//tx
+
 const char * ssid = "Realme 7";
 const char * password = "Darshnik";
 
@@ -16,11 +51,6 @@ FirebaseData firebaseData;
 
 String path = "/device";
 String path1 = "/Dhruval";
-void printResult(FirebaseData &data);
-void printResult(StreamData &data);
-void connect_wifi();
-void firmwareUpdate();
-int FirmwareVersionCheck();
 
 unsigned long sendDataPrevMillis = 0;
 uint16_t count = 0;
@@ -57,7 +87,7 @@ const char * rootCACertificate = \
 #endif
 
 String FirmwareVer = {
-  "2.1"
+  "2.2"
 };
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
@@ -188,16 +218,318 @@ void streamTimeoutCallback(bool timeout)
     Serial.println();
   }
 }
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+String readFile(fs::FS &fs, const char * path) {
+  Serial.printf("Reading file: %s\r\n", path);
+  String ret = "";
+  int ss[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  int cou = 0;
+  File file = fs.open(path);
+  if (!file || file.isDirectory()) {
+    Serial.println("- failed to open file for reading");
+    return ret;
+  }
+
+  //Serial.println("- read from file:");
+  while (file.available()) {
+    char c = file.read();
+    //    if (cou < 6) {
+//     Serial.println(c);
+    ss[cou] = c- '0';;
+    Serial.println("s" + String(cou) + " - " + String(ss[cou]) );
+    cou = cou + 1;
+    
+    //    }
+    //    //Serial.write(c);
+    ret += c;
 
 
-void setup() {
-  pinMode(button_boot.PIN, INPUT);
-  attachInterrupt(button_boot.PIN, isr, RISING);
-  Serial.begin(115200);
-  Serial.print("Active firmware version:");
-  Serial.println(FirmwareVer);
-  pinMode(ledPin, OUTPUT);
-  connect_wifi();
+  }
+  //Serial.println("");
+  file.close();
+  digitalWrite(lightout, ss[0]);
+  digitalWrite(fanout, ss[1]);
+  digitalWrite(portout, ss[2]);
+  digitalWrite(ledout, ss[3]);
+  digitalWrite(signalout1, ss[4]);
+  digitalWrite(signalout2, ss[5]);
+  return ret;
+}
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("- file renamed");
+    } else {
+        Serial.println("- rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    Serial.printf("Testing file I/O with %s\r\n", path);
+
+    static uint8_t buf[512];
+    size_t len = 0;
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    Serial.print("- writing" );
+    uint32_t start = millis();
+    for(i=0; i<2048; i++){
+        if ((i & 0x001F) == 0x001F){
+          Serial.print(".");
+        }
+        file.write(buf, 512);
+    }
+    Serial.println("");
+    uint32_t end = millis() - start;
+    Serial.printf(" - %u bytes written in %u ms\r\n", 2048 * 512, end);
+    file.close();
+
+    file = fs.open(path);
+    start = millis();
+    end = start;
+    i = 0;
+    if(file && !file.isDirectory()){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        Serial.print("- reading" );
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            if ((i++ & 0x001F) == 0x001F){
+              Serial.print(".");
+            }
+            len -= toRead;
+        }
+        Serial.println("");
+        end = millis() - start;
+        Serial.printf("- %u bytes read in %u ms\r\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("- failed to open file for reading");
+    }
+}
+
+void IRAM_ATTR handleInterrupt1() {
+//  rep = 0;
+//  fail = 0;
+  c = 1;
+  //  if ((millis() - last_pressed1) > 300) {
+  //    last_pressed1 = millis();
+  lf = !(digitalRead(lightin));
+  digitalWrite(lightout,lf);
+   Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+   
+//  if (touch == 0) {
+//    if (lf != l) {
+//      l = lf;
+//      //        registerWrite(8, lf);
+//      //digitalWrite(pin,lf);///////////////example
+//    }
+//  } else {
+//    if (lf != l) {
+//      l = lf;
+//      //        registerWrite(8, !(bitRead (data, 7)));
+//      //digitalWrite(pin,!(digitalRead(pin)));///////////////example
+//    }
+//  }
+  //  }
+
+}
+void IRAM_ATTR handleInterrupt2() {
+  c = 2;
+  //  if ((millis() - last_pressed1) > 300) {
+  //    last_pressed1 = millis();
+  if (sdim > 0) {
+      fq = !(digitalRead(fanin));
+      Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+      fa = fq;
+      if (fa == LOW) {
+
+      } else {
+        //          registerWrite(1, 1);
+        if (ff == 2) {
+
+        } else if (ff == 3) {
+
+        } else if (ff == 4) {
+
+        } else if (ff == 5) {
+
+        }
+      }
+  } else {
+    fq = !(digitalRead(fanin));
+    digitalWrite(fanout,fq);
+    Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+    }
+}
+
+void IRAM_ATTR handleInterrupt3() {
+  c = 3;
+  //  if ((millis() - last_pressed1) > 300) {
+  //    last_pressed1 = millis();
+  lef =  !(digitalRead(ledin));
+  digitalWrite(ledout,lef);
+  Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+//  if (touch == 0) {
+//    if (lef != le) {
+//      le = lef;
+//      //        registerWrite(7, lef);
+//    }
+//  } else {
+//    if (lef != le) {
+//      le = lef;
+//      //        registerWrite(7, !(bitRead (data, 6)));
+//    }
+//  }
+  //  }
+}
+void IRAM_ATTR handleInterrupt4() {
+  c = 4;
+
+  //  if ((millis() - last_pressed1) > 300) {
+  //    last_pressed1 = millis();
+  pf =  !(digitalRead(portin));
+  digitalWrite(portout,pf);
+  Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+  
+//  if (touch == 0) {
+//    if (pf != por) {
+//      por = pf;
+////      registerWrite(6, pf);
+//    }
+//  } else {
+//    if (pf != por) {
+//      por = pf;
+////      registerWrite(6, !(bitRead (data, 5)));
+//    }
+//  }
+  //  }
+}
+void IRAM_ATTR handleInterrupt5() {
+  c = 5;
+  //  if ((millis() - last_pressed1) > 300) {
+  //    last_pressed1 = millis();
+  sigd1 =  !(digitalRead(signalin1));
+  digitalWrite(signalout1,sigd1);
+  Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+//  if (touch == 0) {
+//    if (sigd1 != sig1) {
+//      sig1 = sigd1;
+////      registerWrite(4, sig1);
+//    }
+//  } else {
+//    if (sigd1 != sig1) {
+//      sig1 = sigd1;
+////      registerWrite(4, !(bitRead (data, 3)));
+//    }
+//  }
+  //  }
+}
+void IRAM_ATTR handleInterrupt6() {
+  c=6;
+  sigd2 =  !(digitalRead(signalin2));
+  digitalWrite(signalout2,sigd2);
+  Serial.println(("switch "+String(c) + " out -> " +String(lf)));
+//  if (touch == 0) {
+//    if (sigd2 != sig2) {
+//      sig2 = sigd2;
+////      registerWrite(3, sig2);
+//    }
+//  } else {
+//    if (sigd2 != sig2) {
+//      sig2 = sigd2;
+////      registerWrite(3, !(bitRead (data, 2)));
+//    }
+//  }
+  //   }
+}
+void setup() 
+{
+  if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
+    {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    }
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
@@ -211,6 +543,49 @@ void setup() {
     Serial.println();
   }
 
+  Firebase.setStreamCallback(firebaseData, streamCallback, streamTimeoutCallback);
+  pinMode(lightin, INPUT_PULLUP);//_PULLUP
+  pinMode(fanin, INPUT_PULLUP);
+  pinMode(ledin, INPUT_PULLUP);
+  pinMode(portin, INPUT_PULLUP);
+
+  pinMode(lightout, OUTPUT);
+  pinMode(fanout, OUTPUT);
+  pinMode(portout, OUTPUT);
+  pinMode(ledout, OUTPUT);
+  pinMode(signalout1, OUTPUT);
+  pinMode(signalout2, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(lightin), handleInterrupt1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(fanin), handleInterrupt2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ledin), handleInterrupt3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(portin), handleInterrupt4, CHANGE);
+  
+  if (snormal > 4) {
+    attachInterrupt(digitalPinToInterrupt(signalin1), handleInterrupt5, CHANGE);
+  }  if (snormal > 5) {
+    attachInterrupt(digitalPinToInterrupt(signalin2), handleInterrupt6, CHANGE);
+  }
+  interrupts();  
+    
+   listDir(SPIFFS, "/", 0);
+   readFile(SPIFFS, "/hello.txt");
+   if(c>0)
+   {
+     String sstate = String(digitalRead(ledout)) + String(digitalRead(fanout)) + String(digitalRead(portout)) + String(digitalRead(signalout1)) + String(digitalRead(signalout2)) + String(digitalRead(lightout));
+     int len=sstate.length();
+     const char* state1 = sstate.c_str();
+     writeFile(SPIFFS, "/hello.txt", state1);
+   }
+  Serial.println( "Test complete" );
+  pinMode(button_boot.PIN, INPUT);
+  attachInterrupt(button_boot.PIN, isr, RISING);
+  Serial.begin(115200);
+  Serial.print("Active firmware version:");
+  Serial.println(FirmwareVer);
+  pinMode(ledPin, OUTPUT);
+  connect_wifi();
+  
   Firebase.setStreamCallback(firebaseData, streamCallback, streamTimeoutCallback);
   
 }
